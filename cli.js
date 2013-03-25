e86b3b1ef59801d30d6cacc3dc2f4796677aadec
Nathan Zadoks <nathan@nathan7.eu>
2013-03-25 18:11:18 +0100

    add CLI tool

---
+++ cli.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+var request$ = require('./')
+if (process.argv.length < 3)
+  process.exit(console.error('usage: cheerful <url> [selector]'))
+
+var url = process.argv[2]
+  , selector = process.argv[3] || null
+
+request$(url, function(err, $) {
+  if (err) console.error('error:', err)
+  global.err = err
+  global.$ = $
+  require('repl').start({})
+})
